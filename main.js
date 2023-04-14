b1fa5018f59739c26fc2d12899383abce37364ea
Joao <jonhlhn.surf@gmail.com>
2023-04-13 23:30:44 -0300

    Allow board to be rendered

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const Gameboard = (() => {
+  const cells = document.querySelectorAll('.cell');
   const gameBoard = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
   const getGameBoard = () => gameBoard;
   const changeGameBoard = (position, play) => {
     gameBoard[position] = play;
   };
+  const renderBoard = () => {
+    cells.forEach((cell, index) => {
+      cell.textContent = gameBoard[index];
+    });
+  };
 
-  return { getGameBoard, changeGameBoard };
+  return { getGameBoard, changeGameBoard, renderBoard };
 })();
 
 const player = (state) => ({
